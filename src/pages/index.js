7d0389f8f8aac71edd9745e86346042666061d47
Ananto Ghosh <a0ghosh@gmail.com>
2021-03-03 00:40:11 +0530

    init

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import styles from "./index.module.css";
+
+// markup
+const IndexPage = () => {
+  return (
+    <main className={styles.hello}>
+    </main>
+  )
+}
+
+export default IndexPage
